c807a1854109d82eafb2bd1a7f4a59871d6b5e68
Nathan Hare <me@laspruca.nz>
2021-07-04 11:38:53 +1200

    Add async

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { bfc } from "./compile";
+import "regenerator-runtime/runtime.js";
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-    const web_location = window.atob(window.location.pathname.slice(1));
-    console.log(web_location)
-    const bf_return = bfc(web_location)
-    document.body.innerHTML = '';
-    console.log(bf_return)
+document.addEventListener("DOMContentLoaded", function (event) {
+  const web_location = window.atob(window.location.pathname.slice(1));
+  console.log(web_location);
+  bfc(web_location).then((bf_return) => {
+    document.body.innerHTML = "";
+    console.log(bf_return);
     document.open();
     document.write(bf_return);
     document.close();
+  });
 });
